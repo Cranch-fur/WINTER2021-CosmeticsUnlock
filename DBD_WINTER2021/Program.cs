0e9202828280326f7cc25cfabcb3866081299f83
Cranch <50642181+Cranch-fur@users.noreply.github.com>
2021-12-13 19:06:27 +0300

    [RELEASE]

---
+++ DBD_WINTER2021/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace DBD_WINTER2021
+{
+    class Program
+    {
+        private static void IncreaseConsoleBufferSize()
+        {
+            Console.SetIn(new System.IO.StreamReader(Console.OpenStandardInput(),
+                               Console.InputEncoding,
+                               false,
+                               bufferSize: 1024));
+        }
+        private enum HTTP_StatusCode
+        {
+            NOT_FOUND = 404,
+            BHVRSESSION_INCORRECT = 403,
+            BHVRSESSION_CORRECT = 200,
+            INTERNAL_SOFTWARE_ERROR = 0,
+        }
+
+        static void Main(string[] args)
+        {
+            IncreaseConsoleBufferSize();
+            Console.WriteLine("               ░██████╗███████╗██████╗░██╗░░░██╗███████╗██████╗░███╗░░██╗░█████╗░███╗░░░███╗███████╗");
+            Console.WriteLine("               ██╔════╝██╔════╝██╔══██╗██║░░░██║██╔════╝██╔══██╗████╗░██║██╔══██╗████╗░████║██╔════╝");
+            Console.WriteLine("               ╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝█████╗░░██████╔╝██╔██╗██║███████║██╔████╔██║█████╗░░");
+            Console.WriteLine("               ░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██╔══╝░░██╔══██╗██║╚████║██╔══██║██║╚██╔╝██║██╔══╝░░");
+            Console.WriteLine("               ██████╔╝███████╗██║░░██║░░╚██╔╝░░███████╗██║░░██║██║░╚███║██║░░██║██║░╚═╝░██║███████╗");
+            Console.WriteLine("               ╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝");
+            Console.WriteLine("                         [===] WINTER 2021 - BONE CHILL EVENT COSMETICS UNLOCKER [===]");
+            Console.WriteLine("                                    https://cranchpalace.info/discord.php\n");
+            Console.Write("Specify your platform:\n[1] Steam\n[2] Epic Games\n[3] Microsoft Store\n> ");
+            string SpecifiedPlatform = Console.ReadLine();
+            if (int.TryParse(SpecifiedPlatform, out int SpecifiedPlatformInt))
+            {
+                switch (SpecifiedPlatformInt)
+                {
+                    case 1:
+                        httpwrapper.HTTP_Subdomain = "steam";
+                        httpwrapper.HTTP_Platform = "steam";
+                        break;
+
+                    case 2:
+                        httpwrapper.HTTP_Subdomain = "brill";
+                        httpwrapper.HTTP_Platform = "egs";
+                        break;
+
+                    case 3:
+                        httpwrapper.HTTP_Subdomain = "grdk";
+                        httpwrapper.HTTP_Platform = "grdk";
+                        break;
+
+                    default:
+                        Console.WriteLine("\nPlease, specify correct platform number ( 1 | 2 | 3 )...");
+                        System.Threading.Thread.Sleep(2500); System.Diagnostics.Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nPlease, specify the number as selected platform ( 1 | 2 | 3 )...");
+                System.Threading.Thread.Sleep(2500); System.Diagnostics.Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+            }
+
+
+            Console.Write("\nbhvrSession=");
+            string SpecifiedSession = Console.ReadLine();
+            if (SpecifiedSession.Length < 64 || string.IsNullOrEmpty(SpecifiedSession))
+            {
+                Console.WriteLine("\nPlease, specify correct bhvrSession on next launch...");
+                System.Threading.Thread.Sleep(2500); System.Diagnostics.Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+            }
+
+
+            int SessionCheckResponseCode = httpwrapper.WEBREQUEST_GET(SpecifiedSession);
+            if (SessionCheckResponseCode != 200)
+            {
+                Console.WriteLine($"\nSomething went wrong with bhvrSession: {Enum.GetName(typeof(HTTP_StatusCode), SessionCheckResponseCode)}");
+                System.Threading.Thread.Sleep(2500); System.Diagnostics.Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+            }
+            else
+                Console.WriteLine("[+] bhvrSession accepted & confirmed\n[?] Trying to obtain all event rewards...");
+
+
+            for (int a = 0; a < 32; a++)
+            {
+                int SpecialEventUnlockResponseCode = httpwrapper.WEBREQUEST_POST(SpecifiedSession, a);
+                if (SpecialEventUnlockResponseCode != 200)
+                    Console.WriteLine($"[-] Progress State №{a} for {httpwrapper.HTTP_PlayerRole} not obtained, reason: {Enum.GetName(typeof(HTTP_StatusCode), SpecialEventUnlockResponseCode)}");
+                else Console.WriteLine($"[+] Progress State №{a} for {httpwrapper.HTTP_PlayerRole} successfully obtained!");
+            }
+
+            httpwrapper.HTTP_PlayerRole = "killer";
+            httpwrapper.HTTP_ActionKey = "killerSnowmanDestroyed";
+            for (int b = 0; b < 32; b++)
+            {
+                int SpecialEventUnlockResponseCode = httpwrapper.WEBREQUEST_POST(SpecifiedSession, b);
+                if (SpecialEventUnlockResponseCode != 200)
+                    Console.WriteLine($"[-] Progress State №{b} for {httpwrapper.HTTP_PlayerRole} not obtained, reason: {Enum.GetName(typeof(HTTP_StatusCode), SpecialEventUnlockResponseCode)}");
+                else Console.WriteLine($"[+] Progress State №{b} for {httpwrapper.HTTP_PlayerRole} successfully obtained!");
+            }
+        }
+    }
+}
---
+++ DBD_WINTER2021/httpwrapper.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace DBD_WINTER2021
+{
+    public static class httpwrapper
+    {
+        public static string HTTP_Subdomain, HTTP_Platform, HTTP_PlayerRole = "survivor", HTTP_ActionKey = "survivorSnowmanDestroyed";
+        private static string RandomMatch()
+        {
+            var random = new System.Random();
+            string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, 8)
+                .Select(s => s[random.Next(s.Length)]).ToArray()) + "-" + new string(Enumerable.Repeat(chars, 4)
+                                                                       .Select(s => s[random.Next(s.Length)]).ToArray()) + "-" + new string(Enumerable.Repeat(chars, 4)
+                                                                                                                              .Select(s => s[random.Next(s.Length)]).ToArray()) + "-" + new string(Enumerable.Repeat(chars, 4)
+                                                                                                                                                                                     .Select(s => s[random.Next(s.Length)]).ToArray()) + "-" + new string(Enumerable.Repeat(chars, 12)
+                                                                                                                                                                                                                                            .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static int WEBREQUEST_POST(string bhvrsession, int progression)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://{HTTP_Subdomain}.live.bhvrdbd.com/api/v1/extensions/objectives/updateObjectiveProgression");
+            request.Method = "POST";
+            request.Headers.Add($"x-kraken-client-platform: {HTTP_Platform}");
+            request.Headers.Add($"x-kraken-client-provider: {HTTP_Platform}");
+            request.Headers.Add("x-kraken-client-os: 10.0.22000.1.768.64bit");
+            request.Headers.Add($"x-kraken-client-version: 5.4.1");
+            request.UserAgent = "DeadByDaylight/++DeadByDaylight+Live-CL-506030 Windows/10.0.19044.1.256.64bit";
+            request.Headers.Add($"Cookie: bhvrSession={bhvrsession}");
+            request.ContentType = "application/json";
+
+
+            JObject Progression = 
+                new JObject(
+                    new JProperty("data",
+                        new JObject(
+                            new JProperty("eventId", "Winter2021"),
+                            new JProperty("inGameProgress",
+                                new JArray(
+                                    new JObject(
+                                        new JProperty("amount", 1),
+                                        new JProperty("key", HTTP_ActionKey)))),
+                            new JProperty("matchId", RandomMatch()),
+                            new JProperty("objectiveId", "Snowmen"),
+                            new JProperty("objectiveVersion", progression),
+                            new JProperty("platformVersion", HTTP_Platform),
+                            new JProperty("playerRole", HTTP_PlayerRole))));
+
+#if DEBUG
+            System.Console.WriteLine(Progression.ToString());
+#endif
+
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Progression));
+            request.ContentLength = data.Length;
+            using (Stream requestBody = request.GetRequestStream())
+                requestBody.Write(data, 0, data.Length);
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    if (reader.ReadToEnd().Contains("404"))
+                        return 404;
+                    else return 200;
+                }
+            }
+            catch (WebException exception) {
+                return 0;
+            }
+        }
+        public static int WEBREQUEST_GET(string bhvrsession)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://{HTTP_Subdomain}.live.bhvrdbd.com/api/v1/wallet/currencies");
+            request.Method = "GET";
+            request.Headers.Add($"x-kraken-client-platform: {HTTP_Platform}");
+            request.Headers.Add($"x-kraken-client-provider: {HTTP_Platform}");
+            request.Headers.Add("x-kraken-client-os: 10.0.22000.1.768.64bit");
+            request.Headers.Add($"x-kraken-client-version: 5.4.1");
+            request.UserAgent = "DeadByDaylight/++DeadByDaylight+Live-CL-506030 Windows/10.0.19044.1.256.64bit";
+            request.Headers.Add($"Cookie: bhvrSession={bhvrsession}");
+
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                return (int)response.StatusCode;
+            }
+            catch (WebException exception) {
+                return System.Convert.ToInt32(((HttpWebResponse)exception.Response).StatusCode);
+            }
+        }
+    }
+}
